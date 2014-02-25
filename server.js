2c107a63bcf4e74ae91a8f9e2835a4392f0344e1
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2014-02-25 21:02:14 -0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var serverPort = process.argv.pop();
+serverPort = isNaN(serverPort) ? 3000 : serverPort;
+
+var PeerServer = require('peer').PeerServer;
+var server = new PeerServer({ port: serverPort, debug: true });
+
+console.log('PeerJS Server listening on port ' + serverPort);
