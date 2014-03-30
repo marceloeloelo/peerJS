3e65951439a6274ee8e355ca0d0313cfbdb8758b
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2014-03-30 15:50:36 -0300

    Use rhc port and ip

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var serverPort = process.env.PORT || process.argv.pop();
+var ipAddress  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || process.argv.pop();
 serverPort = isNaN(serverPort) ? 3000 : serverPort;
 
 var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({ port: serverPort, debug: true });
+var server = new PeerServer({ port: serverPort, ip: ipAddress, debug: true });
 
 console.log('PeerJS Server listening on port ' + serverPort);
